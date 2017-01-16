c99f87d7a9231c4a056838ecc0de32e5b1468892
DESKTOP-7D1NSHG\Sathish <p.sathish.cs@gmail.com>
2017-01-15 18:04:21 -0600

    swagger fix swagger Url: http://windows10.server.com:8091/sales/api/swagger-ui.html

--- src/main/java/org/sales/SalesApplication.java
+++ src/main/java/org/sales/SalesApplication.java
@@ -10,8 +10,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SalesApplication {
 	public static void main(String[] args){
 		SpringApplication.run(SalesApplication.class, args);
@@ -20,8 +22,7 @@ public class SalesApplication {
 	public Docket api() {                
 	    return new Docket(DocumentationType.SWAGGER_2)          
 	      .select()
-	      .apis(RequestHandlerSelectors.basePackage("org.sales.controller"))
-	      .paths(PathSelectors.ant("/foos/*"))
+	      .apis(RequestHandlerSelectors.any())
 	      .build()
 	      .apiInfo(apiInfo());
 	}
