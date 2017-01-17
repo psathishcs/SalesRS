cd19046ec7f323e51cfd286cb2d91426cdd9c7f0
DESKTOP-7D1NSHG\Sathish <p.sathish.cs@gmail.com>
2017-01-16 22:01:32 -0600

    Adding the project to Eureka Service Discovery Service.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SalesRS
 # Swagger2 UI 
-   http://windows10.server.com:8091/sales/api/swagger-ui.html
+   http://windows10.server.com:8089/sales/api/swagger-ui.html
--- src/main/java/org/sales/SalesApplication.java
+++ src/main/java/org/sales/SalesApplication.java
@@ -2,9 +2,8 @@ package org.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -13,6 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class SalesApplication {
 	public static void main(String[] args){
